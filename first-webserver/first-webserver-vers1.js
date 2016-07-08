a77252b17df3c32f04e1e8e75684e699f77b5d7b
Kaschniz Raschniz <kaschnizRaschniz@xyz.com>
2016-07-08 16:49:51 +0200

    Init Commit

---
+++ first-webserver/first-webserver-vers1.js
@@ -0,0 +1,1 @@
+console.log("Webserver starting up!!");
---
+++ first-webserver/first-webserver-vers4.js
@@ -0,0 +1,20 @@
+var http = require('http');
+http.createServer(function (request, response){
+    
+    response.writeHead(200, {'content-type' : 'text/html; charset=utf-8'});
+    
+    var body = '<html>' +
+                    '<head>' +
+                        '<meta charset="utf-8">' +
+                        '<title>Node.js></title>' +
+                    '</head>' +
+                    '<body>' +
+                        '<h1 style="color:green">Hello World!</h1>' +
+                    '</body>' +
+                '</html>';
+    
+    response.end(body);
+    
+}).listen(8080, '127.0.0.1');
+
+console.log('Webserver wird ausgeführt!!');
